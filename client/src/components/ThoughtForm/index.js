905dd1f07a23b6d53503b9d6a7b369cfee28e880
Scott Lawan <scottlawan@Scotts-Mac-mini.local>
2021-09-27 16:22:02 -0600

    updating frontend form to all allow user to upload images

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react"; // useRef hook to retrieve the image file uploaded by the user
+// import React, { useRef } from 'react'; // use this hook to retrieve the image file uploaded by the user
 
 const ThoughtForm = () => {
   const [formState, setFormState] = useState({
@@ -6,6 +7,32 @@ const ThoughtForm = () => {
     thought: "",
   });
   const [characterCount, setCharacterCount] = useState(0);
+
+  const fileInput = useRef(null);
+  // handle the image upload by the user
+  const handleImageUpload = event => {
+    event.preventDefault();
+    const data = new FormData(); // declaring interface object, data from FormData
+    data.append('image', fileInput.current.files[0]);
+    // send image file to endpoint with the postImage function
+    const postImage = async () => {
+      try {
+        const res = await fetch('/api/image-upload', {
+          mode: 'cors',
+          method: 'POST',
+          body: data
+        })
+        if (!res.ok) throw new Error(res.statusText); // once respose from image upload endpoint is received, we convert response into JSON object . . .
+        const postResponse = await res.json();
+        setFormState({...formState, image: postResponse.Location}) // public url of the image
+        
+        return postResponse.Location;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    postImage();
+  };
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -62,6 +89,21 @@ const ThoughtForm = () => {
           className="form-input col-12 "
           onChange={handleChange}
         ></textarea>
+        <label className="form-input col-12  p-1">
+          Add an image to your thought: 
+          <input
+            type="file"
+            ref={fileInput}
+            className="form-input p-2"
+          />
+          <button 
+            className="btn" 
+            onClick={handleImageUpload} 
+            type="submit"
+          >
+            Upload
+          </button>
+        </label>
         <button className="btn col-12 " type="submit">
           Submit
         </button>
--- server/utils/params-config.js
+++ server/utils/params-config.js
@@ -6,7 +6,7 @@ const params = fileName => { // receives parameter called, 'fileName'
   const fileType = myFile[myFile.length - 1]; // storing reference to the fileType . . .
 
   const imageParams = { // must define all three properties of imageParams
-    Bucket: 'user-images',
+    Bucket: 'user-images-59e19b8a-22dc-4f94-b4b8-fcea08b113f8', // from Amazon S3 dashboard
     Key: `${uuidv4()}.${fileType}`, // uuidv4() is used to ensure unique file name of this file
     Body: fileName.buffer // the temporary storage container of the image. once buffer has been used, temp. storage space is removed by multer middleware
   };
