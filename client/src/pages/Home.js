3ca97f8d2df73bc5462c023512cdf601c3624bda
Scott Lawan <scottlawan@Scotts-Mac-mini.local>
2021-09-27 12:11:58 -0600

    contecting front end and backend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,23 @@ const Home = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [thoughts, setThoughts] = useState([]);
 
+  useEffect(() => { // request all user's thoughts
+    const fetchData = async () => {
+      try {
+        const res = await fetch('/api/users');
+        const jsonData = await res.json();
+        // sort the array by createdAt property ordered by descending values
+        const data = jsonData.sort((a, b) => (a.createdAt < b.createdAt) ? 1 : -1); // we must sort the data ourselves . . .
+        setThoughts([...data]); // storing the sorted data in the useState
+        setIsLoaded(true);
+        console.log(data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <main>
       <div className="flex-row justify-space-between">
